505576f2277a1c8ec296c2883064def90607e392
Andrew Houghton <houghtonandrew0@gmail.com>
2018-12-31 18:09:49 +1100

    changed app titles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import RiverTable from './Table.js'
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import AppBar from '@material-ui/core/AppBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Aqua Gorilla</h1>
+          <Toolbar>
+            <Typography variant="h4" color="inherit">
+              Aqua Gorilla
+            </Typography>
+          </Toolbar>
           <RiverTable />
         </header>
       </div>
--- src/Table.js
+++ src/Table.js
@@ -4,6 +4,8 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 
 class RiverTable extends Component {
   constructor(props) {
@@ -31,19 +33,23 @@ class RiverTable extends Component {
 
     return (
       <div>
-          <h2>River Levels</h2>
-          <Table style={{ width: 1200 }}>
-            <TableHead>
-              <TableRow>
-                <TableCell>River</TableCell>
-                <TableCell>Height</TableCell>
-                <TableCell>Time</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows}
-            </TableBody>
-          </Table>
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            River Levels
+          </Typography>
+        </Toolbar>
+        <Table style={{ width: 1200 }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>River</TableCell>
+              <TableCell>Height</TableCell>
+              <TableCell>Time</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows}
+          </TableBody>
+        </Table>
       </div>
     );
   }
